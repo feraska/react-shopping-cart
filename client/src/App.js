e6e38c4c5cd2b0a263cb0a834011103c1d322ac3
feraska <45358056+feraska@users.noreply.github.com>
2022-07-30 13:04:22 +0300

    filter-comp

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer/Footer";
 import { useState } from 'react';
 import data from "./data.json"
 import Products from './components/Products/Products';
+import Filter from './components/Filter/Filter';
 
 function App() {
   const[products,setProducts]= useState(data);
@@ -16,7 +17,7 @@ function App() {
       <main>
         <div className='wrapper'>
          <Products products={products}/>
-          <div className='filter-wrapper'>Filter</div>
+         <Filter/>
         </div>
       </main>
       <Footer/>
---
+++ client/src/components/Filter/Filter.js
@@ -0,0 +1,33 @@
+import React from "react"
+import "./Filter.scss"
+const Filter=(props)=>{
+    return(
+        <div className="filter-wrapper">
+            <h2 className="filter-title">Filter</h2>
+            <div className="num-of-products">Number of Products</div>
+            <div className="filter-by-size">
+            <span>Filter</span>
+            <select className="filter-select">
+                <option value="ALL">ALL</option>
+                <option value="XS">XS</option>
+                <option value="S">S</option>
+                <option value="M">M</option>
+                <option value="L">L</option>
+                <option value="XL">XL</option>
+                <option value="XXL">XXL</option>
+            </select>
+            </div>
+
+            <div className="filter-by-size">
+            <span>Order</span>
+            <select className="filter-select">
+                <option value="latest">Latest</option>
+                <option value="lowter">Lowter</option>
+                <option value="hightest">Hightest</option>
+            </select>
+            </div>
+
+        </div>
+    )
+}
+export default Filter
